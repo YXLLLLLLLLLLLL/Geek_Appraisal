2daffb082aeb90534ac5a8fa88efef8e5cbf9945
Your Name <you@example.com>
2022-11-04 22:55:59 +0800

    Modify Geek_Appraisal

--- task1/main.c
+++ task1/main.c
@@ -2,7 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello World!\n");
+	double n = 0;
+	double h = 0.001;
+	double x;
+//	double a;
+	printf("请输入常数a的值: a = 3\n");
+//	scanf(" %lf\n ", &a);
+	double y = 3;
+	printf("-------------------\n");
+	printf("x        y\n");
+	while(n < 25000) {
+		n++;
+		x = n * h;
+		y = y - (h * y);
+		printf("%f %f\n", x, y);
+	}
 	return 0;
 }
 
